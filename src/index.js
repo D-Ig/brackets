86f2e815b8f4b875e99bef362a19487cddf7e005
Denis I <denis.ignatovich.m@gmail.com>
2019-03-14 13:46:38 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const mappedConfig = new Map(bracketsConfig);
+  
+  const isOpeningSymbol = symbol => mappedConfig.has(symbol);
+  
+  const getClosingSymbolFor = symbol => mappedConfig.get(symbol);
+  
+  const isEpmty = stack => (stack.length === 0);
+
+  const stack = [];
+  
+  for (const symbol of str) {
+    if (isOpeningSymbol(symbol) && !stack.includes(symbol)) {
+      const closingSymbol = getClosingSymbolFor(symbol);
+      stack.push(closingSymbol);
+    } else {
+      if (isEpmty(stack)) {
+        return false;
+      };
+      const last = stack.pop();
+      if (symbol !== last) {
+        return false;
+      };
+    }
+  };
+  
+  return isEpmty(stack);
+};
